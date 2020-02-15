6933d63720000bcc65a1781fcc1d8305df87d6cf
enioluciano <eniolucianoo@gmail.com>
2020-02-15 12:15:38 -0300

    configuração Completa

---
+++ src/components/templates/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../css/Header.css';
+//import Topo from '../../assets/img/Topo.jpeg'
+
+
+
+
+function Header() {
+  return (
+      
+    <div>
+        oi
+    </div>
+
+       
+  );
+}
+
+export default Header;
---
+++ src/views/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../components/css/Header.css'
+
+import Header from '../components/templates/Header'
+
+
+function Home() {
+    return (
+      <div >
+        <Header />
+        
+  
+      </div>
+    );
+  }
+  
+  export default Home;
